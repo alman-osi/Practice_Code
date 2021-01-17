2b27d18e4ca157f1850ce8ead6b8776fa3c36fcb
alman-osi <alman-osi@users.noreply.github.com>
2021-01-17 12:27:12 -0500

    Lesson 2
    Let vs Var usage
    Const vs Var usage

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ function printName(params) {
 }
 printName() //called outside of function
 
+//'let' vs 'var'
+function Find() {
+    let x = 1
+    console.log('show me rock ' + x) //x = 1 
+    {
+        let x = 2
+        console.log('show me rock, again ' + x) //x = 2
+    }
+}
+Find()
+
+//'const' vs 'var
+const cue = [1,2,3,5] // Cannot be changed once declared
+
+cue = [1,2,4,8] //Gives error
