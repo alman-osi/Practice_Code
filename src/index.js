18fda54f513072cf65200286d451de3a4a19cdaa
alman-osi <alman-osi@users.noreply.github.com>
2021-01-17 12:55:50 -0500

    Deeply Equals defined
    Exaplaines the meaning of '===' as Deeply Equals

--- src/index.js
+++ src/index.js
@@ -11,17 +11,17 @@ var movement = 'Sonata in D Minor'
 var where = 'here'
 //Function Scope
 function printName(params) {
-    console.log('No Name is ' + where)
+    console.log("No Name is " + where)
 }
 printName() //called outside of function
 
 //'let' vs 'var'
 function Find() {
     let x = 1
-    console.log('show me rock ' + x) //x = 1 
+    console.log("show me rock " + x) //x = 1 
     {
         let x = 2
-        console.log('show me rock, again ' + x) //x = 2
+        console.log("how me rock, again " + x) //x = 2
     }
 }
 Find()
@@ -29,4 +29,12 @@ Find()
 //'const' vs 'var
 const cue = [1,2,3,5] // Cannot be changed once declared
 
-cue = [1,2,4,8] //Gives error
+//cue = [1,2,4,8] //Gives error
+
+const a = 8
+if (a === 8) { //Deeply Equals means 'a' is common in both Value and Type
+    console.log("Yes")
+} 
+else {
+    console.log("THERE'S NO SHOT!")
+}
