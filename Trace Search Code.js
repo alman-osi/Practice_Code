3e5fe4e468c9925d155a6499e9e850f514ae7921
alman-osi <alman-osi@users.noreply.github.com>
2021-02-19 20:03:05 -0500

    Trace Search Code
    This code is used to understand how the Trace oepration works in Command Lines.

---
+++ Trace Search Code.js
@@ -0,0 +1,11 @@
+let routerNum = 10;
+
+for (ttl = 1; routerNum > ttl; ttl++){
+    for(routerNum = 1; routerNum != ttl; routerNum++){
+
+        console.log("the Time To Live is " + ttl + 
+        ", which has linked to the router number " + routerNum);
+        
+        console.log("/n");
+    }    
+}
