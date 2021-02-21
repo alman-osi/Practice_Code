2b46d6ad6e0a48e03e60f9d2b92c80e7a1ea2f30
alman-osi <alman-osi@users.noreply.github.com>
2021-02-20 19:18:58 -0500

    New Class; Switch statement Error
    New Class has been created to hold all aspects relating to the players of this card game. "Deal()" has been reverted to a simplified version.
    Switch case is needing debugging since 'default' case is always the outcome now.

--- CardGame/Card_Trash.js
+++ CardGame/Card_Trash.js
@@ -1,10 +1,10 @@
-class Card{
+class Card {
     constructor(suit, value){ //initallize/creates a card
         this.suit = suit;
         this.value = value;
     }
 }
-class Deck{
+class Deck {
     constructor(){ //initallizes a deck via array
         this.Deck = [];
     }
@@ -30,11 +30,51 @@ class Deck{
         }
         return this.Deck;
     }
+    Deal(){
+            let hand = [];
+            while(hand.length < 10){
+                hand.push(this.Deck.pop());
+            }
+            return hand;
+    }
+}
+class thePlayers extends Deck{
+    constructor(player_Amnt){
+        super();
+        this.player_Amnt = player_Amnt;
+    }
     playerBase() { //input the amount of players here
-        let player = prompt("Please enter amount of players");
-        if (player != null) {
-            if (2 <= player && player <= 6) {
-                console.log("Success! " + player + " players are now in the game");
+        let player_Amnt = prompt("Please enter amount of players");
+        if (player_Amnt != null) {
+            if (2 <= player_Amnt && player_Amnt <= 6) {
+                console.log("Success! " + player_Amnt + " players are now in the game");
+                switch (player_Amnt) {
+                    case 2:
+                        deck.createDeck(suit, value);
+                        console.log("One deck made available");
+                        break;
+                    case 3:
+                    case 4:
+                        deck.createDeck(suit, value);
+                        deck.createDeck(suit, value);
+                        console.log("Two decks made available");
+                    
+                        break;
+                    case 5:
+                    case 6:
+                        deck.createDeck(suit, value);
+                        deck.createDeck(suit, value);
+                        deck.createDeck(suit, value);
+                        console.log("Three decks made available");
+                        break;
+                    default:
+                        console.log("Player number entered for this game is invalid. Select 2-6 Players.");
+                        break;
+                }
+                for (let players = 0; players < player_Amnt; players++){
+                    console.log(deck.Deal()); 
+                    console.log("Here are your 10 cards, players.");
+                }
             }
             else {
                 console.log("Choose an amount between 2 and 6 players!");
@@ -45,47 +85,15 @@ class Deck{
          * element = "demo" + x;
          * document.getElementById(element).innerHTML = Deal();} */
     }
-    Deal(){
-        for (player_Amnt = player; player < player_Amnt; player++){
-            let hand = [];
-            while(hand.length < 10){
-                hand.push(this.Deck.pop());
-            }
-            return hand;
-        }   
-    }
 }
 let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let suit = ["Spade", "Diamond", "Clover", "Hearts"];
 
 //'deck' in CONSOLE is always LOWER CASE via 'let' command below
 let deck = new Deck(); //New deck is created 
-let player_Amnt = 1; //player amount selected
-switch (player_Amnt) {
-    case 2:
-        deck.createDeck(suit, value);
-        console.log("One deck made available");
-        break;
-    case 3:
-    case 4:
-        deck.createDeck(suit, value);
-        deck.createDeck(suit, value);
-        console.log("Two decks made available");
-       
-        break;
-    case 5:
-    case 6:
-        deck.createDeck(suit, value);
-        deck.createDeck(suit, value);
-        deck.createDeck(suit, value);
-        console.log("Three decks made available");
-        break;
-    default:
-        console.log("Player number entered for this game is invalid. Select 2-6 Players.");
-        break;
-}
+let playerbase = new thePlayers();
+
 deck.Shuffle();
 deck.Shuffle();
-console.log(deck.Deal()); 
-console.log("Here are your 10 cards, players.");
+
 //for-loop for player's scores or end results
